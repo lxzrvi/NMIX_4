1e91e3e32fb6b66f470e5c6434104815d0d0a495
lxzrvi <lxzrvi@gmail.com>
2026-08-20 11:26:01 +0000

    Fix MainActivity

--- app/src/main/java/com/lxzrvi/nmix/MainActivity.kt
+++ app/src/main/java/com/lxzrvi/nmix/MainActivity.kt
@@ -18,7 +18,8 @@ import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        setContent {
             NmixApp()
         }
     }
