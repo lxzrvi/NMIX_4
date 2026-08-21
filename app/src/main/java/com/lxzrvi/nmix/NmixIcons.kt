90cc0c6a4959e96a9dbb2119919ef5cf0c40a176
lxzrvi <lxzrvi@gmail.com>
2026-08-21 20:51:25 +0530

    Update NmixIcons.kt

--- app/src/main/java/com/lxzrvi/nmix/NmixIcons.kt
+++ app/src/main/java/com/lxzrvi/nmix/NmixIcons.kt
@@ -14,7 +14,8 @@ enum class NmixIcon{
     CALCULATOR,CLOCK,COUNTER,HELP,TIMER,STOPWATCH,
     ARROW_UP,ARROW_DOWN,CHEVRON_DOWN,MENU,CLOSE,
     FULLSCREEN,BACK,PLUS,MINUS,RESET,RANDOM,
-    DECIMAL,PLUS_MINUS,BACKSPACE,CHECK
+    DECIMAL,PLUS_MINUS,BACKSPACE,CHECK,
+    ROTATE,WALLPAPER
 }
 
 @Composable
@@ -166,10 +167,13 @@ fun NmixIcon(
             NmixIcon.FULLSCREEN->{
                 line(.20f,.39f,.20f,.20f)
                 line(.20f,.20f,.39f,.20f)
+
                 line(.61f,.20f,.80f,.20f)
                 line(.80f,.20f,.80f,.39f)
+
                 line(.20f,.61f,.20f,.80f)
                 line(.20f,.80f,.39f,.80f)
+
                 line(.61f,.80f,.80f,.80f)
                 line(.80f,.80f,.80f,.61f)
             }
@@ -183,23 +187,28 @@ fun NmixIcon(
             NmixIcon.RESET->{
                 val path=Path().apply{
                     moveTo(w*.72f,h*.34f)
+
                     cubicTo(
                         w*.61f,h*.20f,
                         w*.41f,h*.17f,
                         w*.28f,h*.29f
                     )
+
                     cubicTo(
                         w*.12f,h*.43f,
                         w*.18f,h*.70f,
                         w*.36f,h*.79f
                     )
+
                     cubicTo(
                         w*.50f,h*.86f,
                         w*.67f,h*.79f,
                         w*.73f,h*.67f
                     )
                 }
+
                 drawPath(path,color,style=stroke)
+
                 line(.72f,.34f,.72f,.18f)
                 line(.72f,.34f,.56f,.34f)
             }
@@ -208,22 +217,26 @@ fun NmixIcon(
                 val top=Path().apply{
                     moveTo(w*.18f,h*.31f)
                     lineTo(w*.31f,h*.31f)
+
                     cubicTo(
                         w*.42f,h*.31f,
                         w*.55f,h*.69f,
                         w*.69f,h*.69f
                     )
+
                     lineTo(w*.81f,h*.69f)
                 }
 
                 val bottom=Path().apply{
                     moveTo(w*.18f,h*.69f)
                     lineTo(w*.31f,h*.69f)
+
                     cubicTo(
                         w*.42f,h*.69f,
                         w*.55f,h*.31f,
                         w*.69f,h*.31f
                     )
+
                     lineTo(w*.81f,h*.31f)
                 }
 
@@ -232,6 +245,7 @@ fun NmixIcon(
 
                 line(.70f,.59f,.81f,.69f)
                 line(.70f,.79f,.81f,.69f)
+
                 line(.70f,.21f,.81f,.31f)
                 line(.70f,.41f,.81f,.31f)
             }
@@ -255,7 +269,9 @@ fun NmixIcon(
                     lineTo(w*.35f,h*.70f)
                     close()
                 }
+
                 drawPath(path,color,style=stroke)
+
                 line(.48f,.41f,.64f,.59f)
                 line(.64f,.41f,.48f,.59f)
             }
@@ -263,6 +279,82 @@ fun NmixIcon(
             NmixIcon.CHECK->{
                 line(.25f,.52f,.43f,.69f)
                 line(.43f,.69f,.76f,.32f)
+            }
+
+            NmixIcon.ROTATE->{
+                val top=Path().apply{
+                    moveTo(w*.25f,h*.43f)
+
+                    cubicTo(
+                        w*.29f,h*.25f,
+                        w*.44f,h*.18f,
+                        w*.58f,h*.21f
+                    )
+
+                    cubicTo(
+                        w*.66f,h*.22f,
+                        w*.72f,h*.27f,
+                        w*.77f,h*.33f
+                    )
+                }
+
+                drawPath(top,color,style=stroke)
+
+                line(.77f,.33f,.77f,.18f)
+                line(.77f,.33f,.62f,.33f)
+
+                val bottom=Path().apply{
+                    moveTo(w*.75f,h*.57f)
+
+                    cubicTo(
+                        w*.71f,h*.75f,
+                        w*.56f,h*.82f,
+                        w*.42f,h*.79f
+                    )
+
+                    cubicTo(
+                        w*.34f,h*.78f,
+                        w*.28f,h*.73f,
+                        w*.23f,h*.67f
+                    )
+                }
+
+                drawPath(bottom,color,style=stroke)
+
+                line(.23f,.67f,.23f,.82f)
+                line(.23f,.67f,.38f,.67f)
+            }
+
+            NmixIcon.WALLPAPER->{
+                val frame=Path().apply{
+                    moveTo(w*.20f,h*.22f)
+                    lineTo(w*.80f,h*.22f)
+                    lineTo(w*.80f,h*.78f)
+                    lineTo(w*.20f,h*.78f)
+                    close()
+                }
+
+                drawPath(
+                    frame,
+                    color,
+                    style=stroke
+                )
+
+                dot(.63f,.38f,.055f)
+
+                val mountains=Path().apply{
+                    moveTo(w*.25f,h*.68f)
+                    lineTo(w*.40f,h*.50f)
+                    lineTo(w*.51f,h*.61f)
+                    lineTo(w*.59f,h*.52f)
+                    lineTo(w*.75f,h*.68f)
+                }
+
+                drawPath(
+                    mountains,
+                    color,
+                    style=stroke
+                )
             }
         }
     }
