b3d2b6f4aba260de3a4df667191625331a9885ff
lxzrvi <lxzrvi@gmail.com>
2026-08-22 22:43:51 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,10 +10,12 @@ android {
 
     defaultConfig {
         applicationId = "com.lxzrvi.nmix"
+
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+
+        versionCode = 10
+        versionName = "1.0.10"
     }
 
     compileOptions {
@@ -31,11 +33,37 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
-    implementation("androidx.activity:activity-compose:1.10.0")
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2024.12.01"
+        )
+    )
+
+    implementation(
+        "androidx.activity:activity-compose:1.10.0"
+    )
+
+    implementation(
+        "androidx.core:core-ktx:1.15.0"
+    )
+
+    implementation(
+        "androidx.core:core-splashscreen:1.0.1"
+    )
+
+    implementation(
+        "androidx.compose.ui:ui"
+    )
+
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
 }
