a731feccad63b2f7033a8528c4e87501727c4044
lxzrvi <lxzrvi@gmail.com>
2026-08-23 19:58:36 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,10 @@ plugins {
 }
 
 val keystoreProperties=Properties()
-val keystoreFile=rootProject.file("keystore.properties")
+val keystorePropertiesFile=rootProject.file("keystore.properties")
 
-if(keystoreFile.exists()){
-    keystoreProperties.load(
-        keystoreFile.inputStream()
-    )
+if(keystorePropertiesFile.exists()){
+    keystoreProperties.load(keystorePropertiesFile.inputStream())
 }
 
 android {
@@ -23,23 +21,19 @@ android {
         applicationId="com.lxzrvi.nmix"
         minSdk=26
         targetSdk=35
-
         versionCode=10
         versionName="1.0.10"
     }
 
     signingConfigs {
         create("release"){
-            if(keystoreFile.exists()){
+            if(keystorePropertiesFile.exists()){
                 storeFile=rootProject.file(
                     keystoreProperties.getProperty("storeFile")
                 )
-                storePassword=
-                    keystoreProperties.getProperty("storePassword")
-                keyAlias=
-                    keystoreProperties.getProperty("keyAlias")
-                keyPassword=
-                    keystoreProperties.getProperty("keyPassword")
+                storePassword=keystoreProperties.getProperty("storePassword")
+                keyAlias=keystoreProperties.getProperty("keyAlias")
+                keyPassword=keystoreProperties.getProperty("keyPassword")
             }
         }
     }
@@ -47,11 +41,7 @@ android {
     buildTypes {
         getByName("release"){
             isMinifyEnabled=false
-
-            if(keystoreFile.exists()){
-                signingConfig=
-                    signingConfigs.getByName("release")
-            }
+            signingConfig=signingConfigs.getByName("release")
         }
     }
 
@@ -70,37 +60,12 @@ android {
 }
 
 dependencies {
-    implementation(
-        platform(
-            "androidx.compose:compose-bom:2024.12.01"
-        )
-    )
-
-    implementation(
-        "androidx.activity:activity-compose:1.10.0"
-    )
-
-    implementation(
-        "androidx.core:core-ktx:1.15.0"
-    )
-
-    implementation(
-        "androidx.core:core-splashscreen:1.0.1"
-    )
-
-    implementation(
-        "androidx.compose.ui:ui"
-    )
-
-    implementation(
-        "androidx.compose.ui:ui-tooling-preview"
-    )
-
-    implementation(
-        "androidx.compose.material3:material3"
-    )
-
-    debugImplementation(
-        "androidx.compose.ui:ui-tooling"
-    )
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
